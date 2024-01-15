8352ccb2a570a8c59cd69ad1b937751705bff60f
Hantonny Korrea <hantonny.korrea@nastek.com.br>
2024-01-15 18:00:13 -0400

    Carrinho

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,5 @@
+
 class Constants {
   static const PRODUCT_BASE_URL = 'https://shop-cod3r-8526d-default-rtdb.firebaseio.com/products';
+  static const ORDER_BASE_URL = 'https://shop-cod3r-8526d-default-rtdb.firebaseio.com/orders';
 }
