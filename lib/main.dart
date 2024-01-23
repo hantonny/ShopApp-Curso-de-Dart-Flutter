cd24ab207bb37d9fff53d5d921b8650998d089e4
Hantonny Korrea <hantonny.korrea@nastek.com.br>
2024-01-23 16:20:22 -0400

    Associar Pedido ao Usuário

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,8 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProxyProvider<Auth, OrderList>(
           create: (_) => OrderList(),
           update: (ctx, auth, previous) {
-            return OrderList(auth.token ?? '', previous?.items ?? []);
+            return OrderList(
+                auth.token ?? '', auth.userId ?? '', previous?.items ?? []);
           },
         ),
         ChangeNotifierProvider(
